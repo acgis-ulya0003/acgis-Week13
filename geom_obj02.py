cc27c95aae9884fe70f56adafbf4e25628a4669e
Andrei Ulyanov <ulya0003@algonquinlive.com>
2021-04-09 19:00:03 -0400

    geom_obj02.py complete

--- geom_obj02.py
+++ geom_obj02.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from time import perf_counter
 
 def main():
     if len(sys.argv) != 3:
@@ -7,6 +8,8 @@ def main():
         sys.exit(0)
 
 import arcpy
+
+t1_start = perf_counter()
 
 sf = sys.argv[2]
 path = os.path.dirname(sf)
@@ -16,9 +19,50 @@ arcpy.env.workspace = path
 arcpy.env.overwriteOutput = True
 
 arcpy.CreateFeatureclass_management(path, fc, 'Polyline', spatial_reference = 4269)
-
 with open(sys.argv[1], mode = 'r') as in_file, \
      arcpy.da.InsertCursor(sf, ["SHAPE@"]) as cursor:
+    
+    
+    point_list = []
+    wk_string = 'LINESTRING ('
+
+    next(in_file)
+    for row in in_file:
+  
+        
+
+        row = row.strip('\n')
+        
+
+        if len(row.split(' ')) > 1:
+            
+            longitude, latitude = row.split(' ')
+            wk_string += longitude + ' ' + latitude + ', '
+        
+        else:
+            wk_string = wk_string[:-2] + ')'                       
+            spatial_reference = arcpy.SpatialReference(4269)
+            polyline = arcpy.FromWKT(wk_string, spatial_reference)            
+            cursor.insertRow([polyline])
+            wk_string = 'LINESTRING ('
+            
+            
+    wk_string = wk_string[:-2] + ')'
+    spatial_reference = arcpy.SpatialReference(4269)
+    polyline = arcpy.FromWKT(wk_string, spatial_reference)            
+    cursor.insertRow([polyline])
+    
+    
+
+t2_stop = perf_counter()
+print('\n')
+print('Elapsed time: ' + str(t2_stop - t1_start ))
+print('\n')
+    
+                    
+            
 
 if __name__ == '__main__':
     main()
+
+#python geom_obj02.py E:\Documents\acgis\data\Canada\canada.txt E:\Documents\acgis\data\Canada\Canada_lines_wkt.shp
